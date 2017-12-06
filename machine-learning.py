cc5cc2855d77da5243e5c17009f90b8167f51b83
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2017-12-06 23:06:00 +0100

    Added machine learning predicions and cross validation scores

--- machine-learning.py
+++ machine-learning.py
@@ -1,7 +1,14 @@
 #%% Loading libraries and imports
 import pandas as pd
 from sklearn.cross_validation import train_test_split
-from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier
+import sklearn.metrics
+from sklearn import tree, svm
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import cross_val_score
+from sklearn.naive_bayes import GaussianNB
 print('Finished loading libraries and imports')
 
 #%% Loading and merging the data
@@ -14,17 +21,6 @@ print('Finished loading and merging the data')
 #%% Loading functions
 
 
-def get_player_statistics_for_map(match_id, map_id):
-    df_match_stats = df_statistics.loc[df_statistics['MatchID'] == match_id]
-    df_match_stats = df_match_stats.drop(
-        df_match_stats[df_match_stats.Map != map_id].index)
-    return df_match_stats
-
-
-def get_team_by_id(team_id):
-    return df_teams.loc[df_teams['ID'] == team_id]
-
-
 def clean_matches_dataset():
     df = pd.DataFrame()
     df['match_id'] = df_matches['Match ID']
@@ -35,6 +31,7 @@ def clean_matches_dataset():
         df_matches['Team 1 Half 2 Score'] + df_matches['Team 1 Overtime Score']
     df['team_2_score'] = df_matches['Team 2 Half 1 Score'] + \
         df_matches['Team 2 Half 2 Score'] + df_matches['Team 2 Overtime Score']
+    print('Finished cleaning matches dataset')
     return df
 
 
@@ -44,6 +41,7 @@ def decide_winners(dataframe):
             i, 'winning_team_nr', 1 if row['team_1_score'] > row['team_2_score'] else 2)
         dataframe.set_value(
             i, 'winning_team_id', row['team_1_id'] if row['team_1_score'] > row['team_2_score'] else row['team_2_id'])
+    print('Finished deciding winners')
     return dataframe
 
 
@@ -65,35 +63,85 @@ def calculate_winrate_for_team_on_map(df_total, team_id, map_id):
 def calculate_winrate_for_matches(df_total):
     already_calculated_keys = {}
     for i, row in df_total.iterrows():
-        if str(row['map']) + str(row['team_1_id']) not in already_calculated_keys:
+        team_1_key = str(row['map']) + str(row['team_1_id'])
+        team_2_key = str(row['map']) + str(row['team_2_id'])
+        if team_1_key not in already_calculated_keys:
             winrate = calculate_winrate_for_team_on_map(
                 df_total, row['team_1_id'], row['map'])
-            already_calculated_keys[str(
-                row['map']) + str(row['team_1_id'])] = winrate
+            already_calculated_keys[team_1_key] = winrate
             df_total.set_value(i, 'team_1_winrate', winrate)
         else:
-            df_total.set_value(i, 'team_1_winrate', already_calculated_keys[str(
-                row['map']) + str(row['team_1_id'])])
-        if str(row['map']) + str(row['team_2_id']) not in already_calculated_keys:
+            df_total.set_value(i, 'team_1_winrate',
+                               already_calculated_keys[team_1_key])
+        if team_2_key not in already_calculated_keys:
             winrate = calculate_winrate_for_team_on_map(
                 df_total, row['team_2_id'], row['map'])
-            already_calculated_keys[str(
-                row['map']) + str(row['team_2_id'])] = winrate
+            already_calculated_keys[team_2_key] = winrate
             df_total.set_value(i, 'team_2_winrate', winrate)
         else:
-            df_total.set_value(i, 'team_2_winrate', already_calculated_keys[str(
-                row['map']) + str(row['team_2_id'])])
-        print('Calculated winrate for row ' + str(i))
+            df_total.set_value(i, 'team_2_winrate',
+                               already_calculated_keys[team_2_key])
+    print('Finished calculating winrates')
     df_total.to_csv('winrates.csv', index=False)
     return df_total
 
 
 def split_data_train_test(df):
-    x = df['team_1_winrate', 'team_2_winrate'].values
+    x = df[['team_1_winrate', 'team_2_winrate']].values
     y = df['winning_team_nr'].values
     x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=0.3, random_state=1337)
     return {'x_train': x_train, 'x_test': x_test, 'y_train': y_train, 'y_test': y_test}
+
+
+def decision_trees(sets):
+    clf = DecisionTreeClassifier(criterion="gini", random_state=100,
+                                 max_depth=3, min_samples_leaf=5)
+    clf.fit(sets['x_train'], sets['y_train'])
+    y_pred = clf.predict(sets['x_test'])
+    print(
+        f"Decision trees cross validation score is {cross_val_score(clf, df_final_data[['team_1_winrate', 'team_2_winrate']].values, df_final_data['winning_team_nr'].values, cv=5)}")
+
+
+def support_vector_machines(sets):
+    clf = svm.SVC()
+    clf.fit(sets['x_train'], sets['y_train'])
+    y_pred = clf.predict(sets['x_test'])
+    print(
+        f"Support Vector Machines cross validation score is {cross_val_score(clf, df_final_data[['team_1_winrate', 'team_2_winrate']].values, df_final_data['winning_team_nr'].values, cv=5)}")
+
+
+def neural_network_mlp(sets):
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                        hidden_layer_sizes=(5, 2), random_state=1)
+    clf.fit(sets['x_train'], sets['y_train'])
+    y_pred = clf.predict(sets['x_test'])
+    print(
+        f"Neural Network MLP cross validation score is {cross_val_score(clf, df_final_data[['team_1_winrate', 'team_2_winrate']].values, df_final_data['winning_team_nr'].values, cv=5)}")
+
+
+def random_forest(sets):
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf.fit(sets['x_train'], sets['y_train'])
+    y_pred = clf.predict(sets['x_test'])
+    print(
+        f"Random Forest cross validation score is {cross_val_score(clf, df_final_data[['team_1_winrate', 'team_2_winrate']].values, df_final_data['winning_team_nr'].values, cv=5)}")
+
+
+def linear_regression(sets):
+    clf = LinearRegression()
+    clf.fit(sets['x_train'], sets['y_train'])
+    y_pred = clf.predict(sets['x_test'])
+    print(
+        f"Linear Regression cross validation score is {cross_val_score(clf, df_final_data[['team_1_winrate', 'team_2_winrate']].values, df_final_data['winning_team_nr'].values, cv=5)}")
+
+
+def naive_bayes(sets):
+    clf = GaussianNB()
+    clf.fit(sets['x_train'], sets['y_train'])
+    y_pred = clf.predict(sets['x_test'])
+    print(
+        f"Naive Bayes cross validation score is {cross_val_score(clf, df_final_data[['team_1_winrate', 'team_2_winrate']].values, df_final_data['winning_team_nr'].values, cv=5)}")
 
 
 print('Finished loading functions')
@@ -103,4 +151,14 @@ df_cleaned_data = decide_winners(clean_matches_dataset())
 df_final_data = calculate_winrate_for_matches(df_cleaned_data)
 
 #%%
-train_test_sets = split_data_train_test()
+train_test_sets = split_data_train_test(pd.read_csv('winrates.csv'))
+print('Finished splitting test and train data')
+
+#%%
+decision_trees(train_test_sets)
+support_vector_machines(train_test_sets)
+neural_network_mlp(train_test_sets)
+random_forest(train_test_sets)
+linear_regression(train_test_sets)
+naive_bayes(train_test_sets)
+print('Finished getting accuracy of all alghoritms')
